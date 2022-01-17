9a2d7cd4f53351cb7b3b57afd27979d963ce42df
faisoabdirisak <faisoabdirisak@gmail.com>
2022-01-17 18:46:11 +0300

    it will add service

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,5 +1,6 @@
 export class User {
     static result: any;
+  user: string;
     constructor(public url:string, public login:string, public html_url:string,  public location:string,public email:string,public name:string, 
        public public_repos:number, public followers:number, public following:number, public avatar_url:string, public created_at:Date ){
   
--- src/app/userservice.service.ts
+++ src/app/userservice.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { User } from './user';
+import { Repos } from './repos'
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { environment } from 'src/environments/environment.prod';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserserviceService {
+ 
+  user!: User;
+  allRepos!: Repos;
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+    this.user = new User("","",""," ","","",0,0,0,"",new Date);
+    this.allRepos = new Repos("","","",new Date,0,0,"");
+  }
+
+SearchUser(){
+
+  let promise = new Promise<void>((resolve,reject)=>{
+    this.http.get<Response>(environment.apiKey).toPromise().then(()=>{
+      this.user.user = Response.name;
+      resolve()
+    },
+    error=>{
+      
+      console.log(error)
+    
+
+      reject(error)
+    })
+  })
+  return promise
+}
+
+getReopos(searchName: string){
+
+  let promise = new Promise<void>((resolve,reject)=>{
+    this.http.get<Response>(environment.apiKey).toPromise().then(()=>{
+      this.allRepos.allRepo = Response;
+      resolve()
+    },
+    error=>{
+      
+      console.log(error)
+    
+
+      reject(error)
+    })
+  })
+  return promise
+}
+
 }
