50045fbcf1535aa583d03417f21ef686c64607e5
faisoabdirisak <faisoabdirisak@gmail.com>
2022-01-17 18:38:31 +0300

    adds changes

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
+import { UserserviceService } from '../userservice.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,9 +11,34 @@ export class ProfileComponent implements OnInit {
   user!: User;
   repo!: any[];
   User!: Promise<void>;
-  constructor() { }
+  constructor(public myservice:UserserviceService) {
+    this.User=myservice.SearchUser();
+
+   }
+searchs(searchName: any){
+
+
+
+}
+
 
   ngOnInit(): void {
-  }
 
+    interface Responce {
+      url:string,
+      login: string;
+      html_url:string;
+      location:string
+      public_repos:number;
+      followers:number;
+      following:number;
+      avatar_url:string;
+      created_at:Date;
+    }
+
+
+    this.myservice.SearchUser();
+    this.user= this.myservice.user
+    this.searchs('faisoabdirisak');
+  }
 }
