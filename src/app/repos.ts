bdc811d348c960d8ef20f862fdff7e14a3265976
faisoabdirisak <faisoabdirisak@gmail.com>
2022-01-17 13:19:13 +0300

    it will add repo class contents

--- src/app/repos.ts
+++ src/app/repos.ts
@@ -1,2 +1,6 @@
 export class Repos {
+    constructor(public name:string,public html_url:string ,public description:string, public created_at:Date
+        , public forks:number,public watchers:number, public language:string){
+      }
 }
+
