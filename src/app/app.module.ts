2d7321f0c7def1d8f6f39b888c4b5bba87242de5
faisoabdirisak <faisoabdirisak@gmail.com>
2022-01-17 08:34:52 +0300

    it will add directive components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ProfileComponent } from './profile/profile.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
 import { SearchformComponent } from './searchform/searchform.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     ProfileComponent,
     RepositoriesComponent,
     SearchformComponent,
-    NavbarComponent
+    NavbarComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
